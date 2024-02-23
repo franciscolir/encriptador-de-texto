7f472a7938a5f7290dd1608214fee3bb96ea05f8
franciscolir <francisco.lira.b@gmail.com>
2024-02-23 13:06:17 -0300

    incluye boton copiar, modifica parrafo de texto cifrado y crea parrafo informativo. extra. crea funcion copiar

--- app.js
+++ app.js
@@ -1,84 +1,68 @@
 
-let a = "ai";
-let e = "enter";
-let i = "imes";
-let o = "ober";
-let u = "ufat";
-let textoInput = [];
-let arrayDeVocales1=[a,e,i,o,u];
-let arrayDeVocales2=["a","e","i","o","u"];
-let textoCifrado = [];
-let unirTextoCifrado=[];
-//let palabraSeparada =[];
-//let letraSeparada =[];
-//let vocalesSeparadas =[];
-//let arrayDeLetras= [];
+
+let textoInput = [""];
+let codigoCifrar=[
+    ["a","ai"],
+    ["e","enter"],
+    ["i","imes"],
+    ["o","ober"],
+    ["u","ufat"]
+];
+let textoCifrado = [""];
+let unirTextoCifrado=[""];
+
 
 function descifrar() {
     //obtiene texto desde el input
+    //recorre texto de input y reemplaza las coincidencias
     //reemplaza codigo/cifrar por vocal
-    textoInput = document.getElementById('textoInput').value
-    textoInput = textoInput.replaceAll (a,"a")
-    textoInput = textoInput.replaceAll (e,"e")
-    textoInput = textoInput.replaceAll (i,"i")
-    textoInput = textoInput.replaceAll (o,"o")
-    textoInput = textoInput.replaceAll (u,"u")
+    textoInput = document.getElementById('textoInput').value;
+    codigoCifrar.map((element) => textoInput = textoInput.replaceAll (element[1],element[0]));
     mostrarTextoCifrado('p',textoInput);
     document.getElementById('descifrar').setAttribute('disabled','true');
+    document.getElementById('cifrar').setAttribute('disabled','true');
 }
+
 
 function cifrar() {
     //obtiene texto desde el input
     //separa string de input y lo convierte en array separado en palabras
     //recorre array separado en palabras
-    //separa palabra en letras
-    //agrega espacio al final de las palabras
-    //recorre array separado en letras
-    //compara elemento de arrayDeLetras con vocales de arrayDeVocales2 
-    //si elemento es vocal recorre arrayDeVocales2 
-    //busca cual vocal es elemento de letraSeparada 
-    //obtiene indice de vocal correspondiente en arrayDeVocales2
-    //reemplaza vocal por codigo/cifrar utilizando indice de elemento 
-    //de arrayDeVocales2 para utilizar elemento de arrayDeVocales1
-    //detiene el ciclo for cuando encontro vocal y la reemplazo por codigo/cifrar
-    //si no es vocal se incluye en array
-    //une letras separadas formando palabras
+    //comprueba si letra esta incluida en array de vocales/codigo
+    //recorre array codigo/cifrar
+    //si elemento es vocal se reemplaza por su codigo correspondiente
+    //si no es vocal se incluye en array textoCifrado
+    //une array textoCifrado en string unirTextoCifrado
     //elimina texto procesado 
-    textoInput = document.getElementById('textoInput').value;
-    const arrayDeInput= textoInput.split(" ");
-
-        for (let index = 0; index < arrayDeInput.length; index++) {
-            palabraSeparada = arrayDeInput[index];
-            arrayDeLetras = palabraSeparada.split("");
-            arrayDeLetras.push(" ");
-
-            for (let index1 = 0; index1 < arrayDeLetras.length; index1++) {
-            letraSeparada = arrayDeLetras[index1];
-                
-            if (arrayDeVocales2.includes(letraSeparada)) {
-                    
-                    for (let index2 = 0; index2 < arrayDeVocales2.length; index2++) {
-                        vocalesSeparadas = arrayDeVocales2[index2];
-                                                
-                        if (letraSeparada==arrayDeVocales2[index2]) {
-                            cambiaVocalPorClave = textoCifrado.push(arrayDeVocales1[index2])
-                            break;
-                        } 
-                            else {}  
-                    }
-                }   
-                    else {
-                        textoCifrado.push(letraSeparada)
-                        }  
-            }
-        }
     
-    unirTextoCifrado = textoCifrado.join(""); 
-    console.log(unirTextoCifrado);
-    mostrarTextoCifrado('p',unirTextoCifrado);
+    textoInput = document.getElementById('textoInput').value;
+    let palabras = textoInput.split("");
+      
+    for (let i = 0; i < palabras.length; i++) {
+        let palabraEnLetras = palabras[i];
+
+        if (codigoCifrar.some(subArray => subArray.includes(palabraEnLetras)) ){
+                    
+            for (let i = 0; i < codigoCifrar.length; i++) {
+                let codigoCifrarSeparado =codigoCifrar[i];
+   
+                    if (codigoCifrarSeparado.includes(palabraEnLetras)) {
+                        cambioCodigoLetra= palabraEnLetras.replace (codigoCifrarSeparado[0],codigoCifrarSeparado[1]);
+                        textoCifrado.push(cambioCodigoLetra);
+                    }                    
+                }
+        } 
+        else {
+            textoCifrado.push(palabraEnLetras);
+            }  
+    }
+    unirTextoCifrado=textoCifrado.join("");
+    mostrarTextoCifrado("#textoDesCifrado",unirTextoCifrado);
     document.getElementById('cifrar').setAttribute('disabled','true');
+    document.getElementById('descifrar').setAttribute('disabled','true');
+    //condicionesIniciales()
     textoCifrado=[""];
-}
+};
 
 function condicionesIniciales(){
     //se inicia conun click en el input
@@ -87,13 +71,25 @@ function condicionesIniciales(){
     //muestra mensaje inicial
     document.getElementById('cifrar').removeAttribute('disabled');
     document.getElementById('descifrar').removeAttribute('disabled');
-    document.getElementById('textoInput').value = "";
-    mostrarTextoCifrado('p',"Ingrese el texto para cifrar o descifrar. Recuerda solo utilizar minusculas, sin puntos o simbolos.") 
-}
+    //document.getElementById('textoInput').value = "";
+    //mostrarTextoCifrado("#textoInformativo","Ingrese el texto para cifrar o descifrar. Recuerda solo utilizar minusculas, sin puntos o simbolos.");
+};
 
 function mostrarTextoCifrado(elemento, texto) {
     //mostrar el texto cifrado o descifrado
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
     return;
-}
+};
+
+function copiarTexto() {
+    
+    let copiar = document.getElementById("textoDesCifrado");
+    let texto = copiar.innerText;
+    let textarea = document.createElement('textarea');
+    textarea.value = texto;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea); 
+};
